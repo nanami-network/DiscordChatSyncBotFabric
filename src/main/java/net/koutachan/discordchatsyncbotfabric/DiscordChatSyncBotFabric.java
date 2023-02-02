f55cd86703519974382b5819700a8cf6d7631ce9
tina-chan <kassi_0123@kassi-hp.info>
2023-02-02 23:45:52 +0900

    1.19暫定対応 (tellコマンド打たれたときの対策ができていない)

--- src/main/java/net/koutachan/discordchatsyncbotfabric/DiscordChatSyncBotFabric.java
+++ src/main/java/net/koutachan/discordchatsyncbotfabric/DiscordChatSyncBotFabric.java
@@ -1,15 +1,23 @@
 package net.koutachan.discordchatsyncbotfabric;
 
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import net.fabricmc.api.ModInitializer;
 import net.koutachan.discordchatsyncbotfabric.discord.DiscordEvent;
+import net.koutachan.discordchatsyncbotfabric.util.CommonUtils;
 import net.koutachan.discordchatsyncbotfabric.util.ConfigManager;
+import net.fabricmc.fabric.api.message.v1.ServerMessageDecoratorEvent;
+import net.koutachan.discordchatsyncbotfabric.util.DiscordUtils;
 
+import java.awt.*;
 import java.nio.file.Paths;
+import java.time.Instant;
+import java.util.concurrent.CompletableFuture;
 
 public class DiscordChatSyncBotFabric implements ModInitializer {
     private static final ConfigManager configManager = new ConfigManager("config.properties", Paths.get("", "config.properties").toAbsolutePath());
@@ -17,7 +25,36 @@ public class DiscordChatSyncBotFabric implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        Runtime.getRuntime().addShutdownHook(new Thread(jda::shutdown));
+        Runtime.getRuntime().addShutdownHook(new Thread(jda::shutdownNow));
+
+        // TODO: なぜかtellコマンド等もこれで捕獲されて中身だけ渡されるのでどうやって検知して止めるか
+        ServerMessageDecoratorEvent.EVENT.register(
+                // Used to provide better compatibility across mods
+                ServerMessageDecoratorEvent.STYLING_PHASE,
+                (sender, message) -> {
+                    TextChannel textChannel = DiscordChatSyncBotFabric.getJDA().getTextChannelById(DiscordChatSyncBotFabric.getConfigManager().getString("textChannelId"));
+
+                    if (sender == null){
+                        return CompletableFuture.completedFuture(message);
+                    }
+
+                    if (sender.getCommandSource() == null){
+                        return CompletableFuture.completedFuture(message);
+                    }
+
+
+
+                    textChannel.sendMessageEmbeds(new EmbedBuilder().setColor(Color.ORANGE)
+                            .setDescription("```" + CommonUtils.translateGoogle(message.getString()) + "```")
+                            .setThumbnail(DiscordUtils.retrieveThumbnail(sender.getCommandSource().getPlayer()))
+                            .setAuthor(sender.getName().getString(), "https://mine.ly/" + sender.getCommandSource().getPlayer().getName().getString())
+                            .setTimestamp(Instant.now())
+                            .build()).queue();
+
+                    return CompletableFuture.completedFuture(message);
+                }
+        );
+
     }
 
     public static ConfigManager getConfigManager() {
--- src/main/java/net/koutachan/discordchatsyncbotfabric/discord/DiscordEvent.java
+++ src/main/java/net/koutachan/discordchatsyncbotfabric/discord/DiscordEvent.java
@@ -6,18 +6,11 @@ import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.impl.game.minecraft.launchwrapper.FabricClassTransformer;
-import net.fabricmc.loader.impl.game.minecraft.launchwrapper.FabricServerTweaker;
-import net.fabricmc.loader.impl.launch.server.FabricServerLauncher;
 import net.koutachan.discordchatsyncbotfabric.DiscordChatSyncBotFabric;
-import net.minecraft.network.MessageType;
-import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
-import net.minecraft.util.Util;
-import org.spongepowered.asm.mixin.FabricUtil;
+//import net.minecraft.text.LiteralText;
 
 public class DiscordEvent extends ListenerAdapter {
     @Override
@@ -38,7 +31,8 @@ public class DiscordEvent extends ListenerAdapter {
                 }
 
                 for (ServerPlayerEntity player : ((MinecraftServer) FabricLoader.getInstance().getGameInstance()).getPlayerManager().getPlayerList()) {
-                    player.sendMessage(new LiteralText("§b[Discord] §r" + message + " (by " + event.getMember().getEffectiveName() + ")"), false);
+                    //player.sendMessage(new LiteralText("§b[Discord] §r" + message + " (by " + event.getMember().getEffectiveName() + ")"), false);
+                    player.sendMessage(Text.literal("§b[Discord] §r" + message + " (by " + event.getMember().getEffectiveName() + ")"), false);
                 }
             }
         }
